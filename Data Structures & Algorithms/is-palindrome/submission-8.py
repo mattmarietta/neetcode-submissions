feae924fd4afc8229611892f05534f692c9bdc6b
Matthew Marietta <129474785+mattmarietta@users.noreply.github.com>
2026-06-25 00:54:05 -0700

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        #Define 2 pointers for traversal
+        pattern = r'[^a-zA-Z0-9]'
+        #Cleans the input string through using replace for whitespace, lower, and removing non alpha chars
+        s = s.replace(" ", "")
+        s = s.lower()
+        s = re.sub(pattern, '', s)
+        print(s)
+        left, right = 0, len(s) - 1
+        is_pali = True
+  
+        while left < right:
+            if s[left] != s[right]:
+                is_pali = False
+                break
+            left += 1
+            right -= 1
+
+        return is_pali
+
