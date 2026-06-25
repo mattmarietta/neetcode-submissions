e305090aa5c16a628531ff9b713e057b6dfbce16
Matthew Marietta <129474785+mattmarietta@users.noreply.github.com>
2026-06-25 00:52:32 -0700

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,23 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Palindrome has same count
+        # Double for loop and remove all the punctuation
+        # 2 pointer
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            while left < right and not self.alphaNum(s[left]):
+                left += 1
+            while right > left and not self.alphaNum(s[right]):
+                right -= 1        
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
