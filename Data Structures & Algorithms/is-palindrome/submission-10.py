5c2afdb1d52592c69b4cf864c046553aa282f140
Matthew Marietta <129474785+mattmarietta@users.noreply.github.com>
2026-06-25 00:55:43 -0700

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.py
@@ -0,0 +1,17 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Palindrome has same count
+        # Double for loop and remove all the punctuation
+        new_word = re.sub(r"[^\w]|_", "", s)
+        new_word = new_word.lower()
+        # 2 pointer
+        left, right = 0, len(new_word) - 1
+
+        while left <= right:
+            if new_word[left] != new_word[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
